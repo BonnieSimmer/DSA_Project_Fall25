a8912415ae6fdab466e6a78a3ca96ea1653dd752
GannaKhaled0 <ganakady12@gmail.com>
2025-12-05 14:03:09 +0200

    Add XMLNode class for XML structure representation

---
+++ include/XMLNode.hpp
@@ -0,0 +1,29 @@
+#ifndef XMLNODE_H
+#define XMLNODE_H
+#include <bits/stdc++.h>
+#include <memory>
+
+using namespace std;
+
+class XMLNode : public enable_shared_from_this<XMLNode> {
+public:
+    string tagname;
+    map<string, string> attributes;
+    string text;
+    vector<shared_ptr<XMLNode>>children;
+    weakptr<XMLNode>parent;
+    int lineNumber; //for error tracking
+
+    XMLNode(string &name, int line_num = 0);
+
+    void setAttribute(string &key, string &value);
+    void setText(string &content);
+    void addChild(shared_ptr<XMLNode> child);
+
+    shared_ptr<XMLNode> getParent() const;
+    vector<shared_ptr<XMLNode>>& getChildren();
+    string getAttribute(const string& name) const;
+
+};
+
+#endif
