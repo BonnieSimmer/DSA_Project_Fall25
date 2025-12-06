eba217aff441266bba95e36881e07ea4d669cb2e
Menna Ayman <mennaaradwan.4@gmail.com>
2025-12-06 12:35:21 +0200

    added command interface class to handle commands without the implementation of functions yet

---
+++ include/CommandLineInterface.hpp
@@ -0,0 +1,14 @@
+#ifndef COMMAND_LINE_INTERFACE_HPP
+#define COMMAND_LINE_INTERFACE_HPP
+
+#include <string>
+
+class CommandLineInterface {
+public:
+    void run(int argc, char* argv[]);
+
+private:
+    
+};
+
+#endif // COMMAND_LINE_INTERFACE_HPP
---
+++ src/CommandLineInterface.cpp
@@ -0,0 +1,47 @@
+#include "CommandLineInterface.hpp"
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+void CommandLineInterface::run(int argc, char* argv[]) {
+
+    if (argc < 3) {
+        std::cout << "Invalid command\n";
+        return;
+    }
+
+    std::string command = argv[1];
+
+    std::string inputFile, outputFile;
+    bool fix = false;
+
+    for(int i = 1; i < argc; i++) {
+        std::string arg = argv[i];
+        if (arg == "-i") inputFile = argv[++i];
+        if (arg == "-o") outputFile = argv[++i];
+        if (arg == "-f") fix = true;
+    }
+
+    // Load file
+    std::ifstream in(inputFile);
+
+    std::vector<std::string> lines;
+    std::string s;
+    while (getline(in, s)) lines.push_back(s);
+
+    if (command == "verify") {
+        // Verify XML
+    }
+    else if (command == "format") {
+        // Format XML
+    }
+    else if (command == "json") {
+       // Convert XML to JSON
+    }
+    else if (command == "mini") {
+        // Minify XML
+    }
+    else {
+        std::cout << "Unknown command\n";
+    }
+}
