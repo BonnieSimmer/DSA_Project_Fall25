1db9f30b59b4cfe4982112476d714df5a55e9a65
Doha Mohamed <dohahabeb15@gmail.com>
2025-12-14 17:18:42 +0200

    Add XMLFormatter

---
+++ include/XMLFormatter.hpp
@@ -0,0 +1,13 @@
+
+
+#ifndef DSA_PROJECT_FALL25_XMLFORMATTER_H
+#define DSA_PROJECT_FALL25_XMLFORMATTER_H
+
+#include <string>
+
+class XMLFormatter {
+public:
+    static std::string format(const std::string& xml);
+};
+
+#endif //DSA_PROJECT_FALL25_XMLFORMATTER_H
