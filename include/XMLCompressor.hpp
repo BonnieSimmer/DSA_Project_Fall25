7fd92377078f0373e3b727f986e54155348de036
GannaKhaled0 <ganakady12@gmail.com>
2025-12-16 17:38:23 +0200

    Add XMLCompressor class with Huffman coding structure

---
+++ include/XMLCompressor.hpp
@@ -0,0 +1,42 @@
+#ifndef XMLCOMPRESSOR_HPP
+#define XMLCOMPRESSOR_HPP
+
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+//using Huffman Coding
+
+// Custom Node for the Huffman Tree
+struct Node {
+    char ch;
+    int freq;
+    Node *left;
+    Node *right;
+
+    Node(char c, int f, Node* l = nullptr, Node* r = nullptr);
+};
+
+// Comparator for the Priority Queue (Min-Heap)
+struct Compare {
+    bool operator()(Node* l, Node* r);
+};
+
+class XMLCompressor {
+private:
+    map<char, string> huffmanCode;
+    map<char, int> freqMap;
+
+    Node* buildTree();
+    void generateCodes(Node* root, string str);
+    void writeCompressedFile(const string& text, const string& outputFile);
+    void deleteTree(Node* root);
+
+public:
+    void compress(const string& inputFile, const string& outputFile);
+};
+
+#endif
