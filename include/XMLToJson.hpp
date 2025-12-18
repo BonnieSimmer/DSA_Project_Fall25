831885a337b7b50890bd9bd40ac29862636a2104
hania mohamed <haniamohamed576@gmail.com>
2025-12-18 11:23:32 +0200

    added XMLToJson

--- include/XMLToJson.hpp
+++ include/XMLToJson.hpp
@@ -1,1 +1,13 @@
+#ifndef XML_TO_JSON_HPP
+#define XML_TO_JSON_HPP
 
+#include <string>
+#include <memory>
+
+/*
+ * Main function to convert an XML string into a formatted JSON string.
+ * This function utilizes the project's XMLParser to build a tree structure and then traverses it to generate JSON.
+ */
+std::string convertXMLToJSON(const std::string& xmlContent);
+
+#endif 
--- src/XMLToJson.cpp
+++ src/XMLToJson.cpp
@@ -1,1 +1,50 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+#include "XMLNode.hpp"
+#include "XMLParsser.hpp"
+
+using namespace std;
+
+void buildJsonFromTree(__shared_ptr<XMLNode> node, int level, string& jsonBuilder, bool isArrayElement){
+    string indent(level *4, ' ');     //indentation for readability
+
+    if(!isArrayElement)
+        jsonBuilder += indent + "\"" + node->tagname + "\": ";
+    else
+        jsonBuilder += indent;
+
+    //case 1:no children
+    if(node->children.empty()){
+        jsonBuilder += "\"" + node->text + "\"";
+        return;
+    }    
+
+    //case2: array of children
+    bool isArray = (node->children.size() > 1) && (node->children[0]->tagname == node->children[1]->tagname);
+
+    if(!isArray){
+        jsonBuilder += "{\n";
+        for(size_t i=0; i<node->children.size(); ++i){
+            buildJsonFromTree(node->children[i], level+1, jsonBuilder, false);
+
+            if(i<node->children.size()-1) jsonBuilder += ",";   //adding comma after every elemnet except last one
+                jsonBuilder += "\n";
+        }
+        jsonBuilder +=indent + "}";
+    }
+    else{
+        jsonBuilder += "{\n" + indent + "    \"" + node->children[0]->tagname + "\": [\n";
+        for (size_t i = 0; i < node->children.size(); ++i) {
+            buildJsonFromTree(node->children[i], level + 2, jsonBuilder, true);
+            
+            if (i < node->children.size() - 1) jsonBuilder += ",";
+            jsonBuilder += "\n";
+        }
+        jsonBuilder += indent + "    ]\n" + indent + "}";
+    }
+}
+
+
 
